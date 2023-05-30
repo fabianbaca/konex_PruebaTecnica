ffbb90bf178c60de1f78b663659b6bb8baea1576
fabianbaca <fabian.baca.14@gmail.com>
2023-05-29 21:40:24 -0500

    1.0.0  - Creacion de back y front

---
+++ Backend_KonexPruebaTecnica/src/main/java/com/fabian/konex_PruebaTecnica/KonexPruebaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.fabian.konex_PruebaTecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonexPruebaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonexPruebaTecnicaApplication.class, args);
+	}
+
+}
---
+++ Backend_KonexPruebaTecnica/src/test/java/com/fabian/konex_PruebaTecnica/KonexPruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabian.konex_PruebaTecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonexPruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
